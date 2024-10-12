70292e0fc1e62988e13258b415723b7d6acc6ddf
caitlinburns <ceburns1@loyola.edu>
2024-10-11 23:24:13 -0400

    initial algorithm and initial code

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-# Code goes here and DO NOT FORGET INTRO COMMENTS
+# Programmers:  Caitlin Burns
+# Course:  CS151, Professor Zee
+# Due Date: 10/16/24
+# Programming Assignment:  PA 4
+# Problem Statement:this code lets users play a game of sticks
+# Data In: number of sticks, numbers from 1-3, and if users want to play again
+# Data Out: total sticks, if users lose, and player 1's choices
+# Credits: this code is based on the assignment given in the read me file
+
 import random
 
 #this code creates a game of sticks between 3 players. the player to select the last stick loses
 
+#this sets the guidlines for the game
 print('Sticks is a game where 3 players must select between 1 and 3 sticks from a pre-selected number of sticks. Player 1 is the computer. The player to select the last stick loses. Good luck!')
 total_sticks = int(input('Enter number of sticks (10-100): '))
 while total_sticks < 10 or total_sticks > 100:
@@ -10,8 +19,7 @@ while total_sticks < 10 or total_sticks > 100:
     total_sticks = int(input('Enter number of sticks (10-100): '))
 print('There are', total_sticks, 'sticks.')
 
-
-
+#this allows player one to go
 print('Player 1 selects first')
 p1_choice = random.randint(1, 3)
 print('Player 1 selects', p1_choice)
@@ -24,7 +32,7 @@ if p1_updated == 0:
 else:
     p1_losses = 0
 
-
+#this allows player 2 to go
 p2_choice = int(input('Player 2 selects next. 1, 2, or 3?: '))
 while p2_choice != 1 and p2_choice != 2 and p2_choice != 3:
     print('Invalid choice')
@@ -39,7 +47,7 @@ if p2_updated == 0:
 else:
     p2_losses = 0
 
-
+#this allows player 3 to go
 p3_choice = int(input('Player 3 selects next. 1, 2, or 3?: '))
 while p3_choice != 1 and p3_choice != 2 and p3_choice != 3:
     print('Invalid choice')
@@ -54,6 +62,7 @@ if p3_updated == 0:
 else:
     p3_losses = 0
 
+#this loops the code until there are no more sticks left
 SENTINEL = 0
 while updated_sticks != SENTINEL:
     p1_choice = random.randint(1, 3)
@@ -98,6 +107,8 @@ while updated_sticks != SENTINEL:
     else:
         p3_losses = 0 + p3_losses
 print('Game over')
+
+#this allows users to decide if they would like to play again or not
 play_again = input('Would you like to play again? (y/n): ')
 SENTINEL = 'n'
 while play_again != SENTINEL:
@@ -193,6 +204,8 @@ while play_again != SENTINEL:
     print('Game over')
     play_again = input('Would you like to play again? (y/n): ')
 print('Thank you for playing.')
+
+#this prints final scores
 print('The final score was: \nPlayer 1:', p1_losses, 'losses \nPlayer 2:', p2_losses, 'losses \nPlayer 3:', p3_losses,'losses')
 
 
