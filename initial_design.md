a44f69c71b62bc3b06e6cc3428a31987ecdbcfe2
caitlinburns <ceburns1@loyola.edu>
2024-10-11 22:33:50 -0400

    initial algorithm and initial code

--- initial_design.md
+++ initial_design.md
@@ -1,2 +1,81 @@
 # Initial Design Document
 #### PLEASE! PLEASE! PLEASE! READ the [README.md](README.md) File carefully
+
+1. import the random module from python
+2. output an introduction explaining the game and that player 1 will be the computer
+3. create a total sticks variable for the user to enter a number of sticks between 10 and 100
+4. while total sticks is less than 10 and greater than 100
+   1. output invalid number
+   2. ask user to enter a number between 10 and 100
+5. output the total number of sticks
+6. output player 1 selects first
+7. set a player 1 choice variable equal to a random int between 1 and 3
+8. output player 1 choice
+9. set an updated sticks variable equal to total sticks minus player 1 choice
+10. set a player 1 updated variable equal to updated sticks 
+11. if player 1 updated is equal to 0
+    1. player 1 losses is equal to 1
+12. otherwise
+    1. player 1 losses is equal to 0
+13. set a player 2 choice variable and have user input 1, 2, or 3 
+14. while player 2 choice is not equal to 1, 2, or 3
+    1. output invalid choice
+    2. ask user to enter 1, 2, or 3
+15. output player 2 selection
+16. set updated sticks equal to updated sticks minus player 2 choice
+17. set player 2 updated equal to updated sticks
+18. if player 2 updated is equal to 0
+    1. player 2 losses is equal to 1
+19. otherwise
+    1. player 2 losses is equal to 0
+20. set a player 3 choice variable and have user input 1, 2, or 3
+21. while player 3 choice is not equal to 1, 2, or 3
+    1. output invalid choice
+    2. ask user to enter 1, 2, or 3
+22. output player 3 selection
+23. set updated sticks equal to updated sticks minus player 3 choice
+24. set player 3 updated equal to updated sticks
+25. if player 3 updated is equal to 0
+    1. player 3 losses is equal to 1
+26. otherwise
+    1. player 3 losses is equal to zero
+27. set SENTINEL equal to 0
+28. while updated sticks is not equal to SENTINEL
+    1. output player 1 selects first
+    2. set a player 1 choice variable equal to a random int between 1 and 3
+    3. output player 1 choice
+    4. set an updated sticks variable equal to updated sticks minus player 1 choice
+    5. set a player 1 updated variable equal to updated sticks 
+    6. if player 1 updated is equal to 0
+        1. player 1 losses is equal to 1 + player 1 losses variable
+    7. otherwise
+        1. player 1 losses is equal to 0 + player 1 losses variable
+    8. set a player 2 choice variable and have user input 1, 2, or 3 
+    9. while player 2 choice is not equal to 1, 2, or 3
+        1. output invalid choice
+        2. ask user to enter 1, 2, or 3
+    10. output player 2 selection
+    11. set updated sticks equal to updated sticks minus player 2 choice
+    12. set player 2 updated equal to updated sticks
+    13. if player 2 updated is equal to 0
+        1. player 2 losses is equal to 1 + player 2 losses variable
+    14. otherwise
+        1. player 2 losses is equal to 0 + player 2 losses variable
+    15. set a player 3 choice variable and have user input 1, 2, or 3
+    16. while player 3 choice is not equal to 1, 2, or 3
+        1. output invalid choice
+        2. ask user to enter 1, 2, or 3
+    17. output player 3 selection
+    18. set updated sticks equal to updated sticks minus player 3 choice
+    19. set player 3 updated equal to updated sticks
+    20. if player 3 updated is equal to 0
+        1. player 3 losses is equal to 1 + player 3 losses variable
+    21. otherwise
+        1. player 3 losses is equal to zero + player 3 losses variable
+29. output game over
+30. ask user if they would like to play again and store answer in a variable
+31. set SENTINEL equal to no
+32. while play again variable is not equal to SENTINEL
+    1. loop entire code again
+33. output thank you for playing
+34. output total losses for each player
--- main.py
+++ main.py
@@ -1,1 +1,201 @@
 # Code goes here and DO NOT FORGET INTRO COMMENTS
+import random
+
+#this code creates a game of sticks between 3 players. the player to select the last stick loses
+
+print('Sticks is a game where 3 players must select between 1 and 3 sticks from a pre-selected number of sticks. Player 1 is the computer. The player to select the last stick loses. Good luck!')
+total_sticks = int(input('Enter number of sticks (10-100): '))
+while total_sticks < 10 or total_sticks > 100:
+    print('Invalid number of sticks')
+    total_sticks = int(input('Enter number of sticks (10-100): '))
+print('There are', total_sticks, 'sticks.')
+
+
+
+print('Player 1 selects first')
+p1_choice = random.randint(1, 3)
+print('Player 1 selects', p1_choice)
+updated_sticks = (total_sticks - p1_choice)
+updated_sticks = int(updated_sticks)
+p1_updated = updated_sticks
+p1_updated = int(p1_updated)
+if p1_updated == 0:
+    p1_losses = 1
+else:
+    p1_losses = 0
+
+
+p2_choice = int(input('Player 2 selects next. 1, 2, or 3?: '))
+while p2_choice != 1 and p2_choice != 2 and p2_choice != 3:
+    print('Invalid choice')
+    p2_choice = int(input('Player 2 selects next. 1, 2, or 3?: '))
+print('Player 2 selects', p2_choice)
+updated_sticks = (updated_sticks - p2_choice)
+updated_sticks = int(updated_sticks)
+p2_updated = updated_sticks
+p2_updated = int(p2_updated)
+if p2_updated == 0:
+    p2_losses = 1
+else:
+    p2_losses = 0
+
+
+p3_choice = int(input('Player 3 selects next. 1, 2, or 3?: '))
+while p3_choice != 1 and p3_choice != 2 and p3_choice != 3:
+    print('Invalid choice')
+    p3_choice = int(input('Player 3 selects next. 1, 2, or 3?: '))
+print('Player 3 selects', p3_choice)
+updated_sticks = (updated_sticks - p3_choice)
+updated_sticks = int(updated_sticks)
+p3_updated = updated_sticks
+p3_updated = int(p3_updated)
+if p3_updated == 0:
+    p3_losses = 1
+else:
+    p3_losses = 0
+
+SENTINEL = 0
+while updated_sticks != SENTINEL:
+    p1_choice = random.randint(1, 3)
+    print('Player 1 selects', p1_choice)
+    updated_sticks = (updated_sticks - p1_choice)
+    updated_sticks = int(updated_sticks)
+    p1_updated = updated_sticks
+    p1_updated = int(p1_updated)
+    p1_losses = 0
+    if p1_updated == 0:
+     p1_losses = 1 + p1_losses
+    else:
+        p1_losses = 0 + p1_losses
+    p2_choice = int(input('Player 2 selects next. 1, 2, or 3?: '))
+    while p2_choice != 1 and p2_choice != 2 and p2_choice != 3:
+        print('Invalid choice')
+        p2_choice = int(input('Player 2 selects next. 1, 2, or 3?: '))
+    print('Player 2 selects', p2_choice)
+    updated_sticks = (updated_sticks - p2_choice)
+    updated_sticks = int(updated_sticks)
+    updated_sticks = updated_sticks
+    p2_updated = updated_sticks
+    p2_updated = int(p2_updated)
+    p2_losses = 0
+    if p2_updated == 0:
+        p2_losses = 1 + p2_losses
+    else:
+        p2_losses = 0 + p2_losses
+
+    p3_choice = int(input('Player 3 selects next. 1, 2, or 3?: '))
+    while p3_choice != 1 and p3_choice != 2 and p3_choice != 3:
+        print('Invalid choice')
+        p3_choice = int(input('Player 3 selects next. 1, 2, or 3?: '))
+    print('Player 3 selects', p3_choice)
+    updated_sticks = (updated_sticks - p3_choice)
+    updated_sticks = int(updated_sticks)
+    p3_updated = updated_sticks
+    p3_updated = int(p3_updated)
+    p3_losses = 0
+    if p3_updated == 0:
+        p3_losses = 1 + p3_losses
+    else:
+        p3_losses = 0 + p3_losses
+print('Game over')
+play_again = input('Would you like to play again? (y/n): ')
+SENTINEL = 'n'
+while play_again != SENTINEL:
+    total_sticks = int(input('Enter number of sticks (10-100): '))
+    while total_sticks < 10 or total_sticks > 100:
+        print('Invalid number of sticks')
+        total_sticks = int(input('Enter number of sticks (10-100): '))
+    print('There are', total_sticks, 'sticks.')
+
+    print('Player 1 selects first')
+    p1_choice = random.randint(1, 3)
+    print('Player 1 selects', p1_choice)
+    updated_sticks = (total_sticks - p1_choice)
+    updated_sticks = int(updated_sticks)
+    p1_updated = updated_sticks
+    p1_updated = int(p1_updated)
+    if p1_updated == 0:
+        p1_losses = 1
+    else:
+        p1_losses = 0
+
+    p2_choice = int(input('Player 2 selects next. 1, 2, or 3?: '))
+    while p2_choice != 1 and p2_choice != 2 and p2_choice != 3:
+        print('Invalid choice')
+        p2_choice = int(input('Player 2 selects next. 1, 2, or 3?: '))
+    print('Player 2 selects', p2_choice)
+    updated_sticks = (updated_sticks - p2_choice)
+    updated_sticks = int(updated_sticks)
+    p2_updated = updated_sticks
+    p2_updated = int(p2_updated)
+    if p2_updated == 0:
+        p2_losses = 1
+    else:
+        p2_losses = 0
+
+    p3_choice = int(input('Player 3 selects next. 1, 2, or 3?: '))
+    while p3_choice != 1 and p3_choice != 2 and p3_choice != 3:
+        print('Invalid choice')
+        p3_choice = int(input('Player 3 selects next. 1, 2, or 3?: '))
+    print('Player 3 selects', p3_choice)
+    updated_sticks = (updated_sticks - p3_choice)
+    updated_sticks = int(updated_sticks)
+    p3_updated = updated_sticks
+    p3_updated = int(p3_updated)
+    if p3_updated == 0:
+        p3_losses = 1
+    else:
+        p3_losses = 0
+
+    SENTINEL = 0
+    while updated_sticks != SENTINEL:
+        p1_choice = random.randint(1, 3)
+        print('Player 1 selects', p1_choice)
+        updated_sticks = (updated_sticks - p1_choice)
+        updated_sticks = int(updated_sticks)
+        p1_updated = updated_sticks
+        p1_updated = int(p1_updated)
+        p1_losses = 0
+        if p1_updated == 0:
+            p1_losses = 1 + p1_losses
+        else:
+            p1_losses = 0 + p1_losses
+        p2_choice = int(input('Player 2 selects next. 1, 2, or 3?: '))
+        while p2_choice != 1 and p2_choice != 2 and p2_choice != 3:
+            print('Invalid choice')
+            p2_choice = int(input('Player 2 selects next. 1, 2, or 3?: '))
+        print('Player 2 selects', p2_choice)
+        updated_sticks = (updated_sticks - p2_choice)
+        updated_sticks = int(updated_sticks)
+        updated_sticks = updated_sticks
+        p2_updated = updated_sticks
+        p2_updated = int(p2_updated)
+        p2_losses = 0
+        if p2_updated == 0:
+            p2_losses = 1 + p2_losses
+        else:
+            p2_losses = 0 + p2_losses
+
+        p3_choice = int(input('Player 3 selects next. 1, 2, or 3?: '))
+        while p3_choice != 1 and p3_choice != 2 and p3_choice != 3:
+            print('Invalid choice')
+            p3_choice = int(input('Player 3 selects next. 1, 2, or 3?: '))
+        print('Player 3 selects', p3_choice)
+        updated_sticks = (updated_sticks - p3_choice)
+        updated_sticks = int(updated_sticks)
+        p3_updated = updated_sticks
+        p3_updated = int(p3_updated)
+        p3_losses = 0
+        if p3_updated == 0:
+            p3_losses = 1 + p3_losses
+        else:
+            p3_losses = 0 + p3_losses
+    print('Game over')
+    play_again = input('Would you like to play again? (y/n): ')
+print('Thank you for playing.')
+print('The final score was: \nPlayer 1:', p1_losses, 'losses \nPlayer 2:', p2_losses, 'losses \nPlayer 3:', p3_losses,'losses')
+
+
+
+
+
